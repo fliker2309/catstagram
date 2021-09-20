244b144c2a54124d6ca5440644fc7a0afae9c26b
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-09-20 21:26:17 +0300

    fix:navigation in activity_main.xml

--- app/src/main/java/com/example/androidtask5network/network/TheCatApiService.kt
+++ app/src/main/java/com/example/androidtask5network/network/TheCatApiService.kt
@@ -2,8 +2,26 @@ package com.example.androidtask5network.network
 
 
 import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.Date
+import androidx.annotation.IntRange
 
 interface TheCatApiService  {
     @GET("images/search")
-    suspend fun getCats(): List<CatResponse>
+    suspend fun getCats(
+        @Query("q") query: String? = null,
+        @Query("page") @IntRange(from = 1) page: Int = 1,
+        @Query("pageSize") @IntRange(from = 1, to = MAX_PAGE_SIZE.toLong()) pageSize: Int = DEFAULT_PAGE_SIZE
+        ): List<CatResponse>
+
+
+
+
+
+
+    companion object {
+
+        const val DEFAULT_PAGE_SIZE = 20
+        const val MAX_PAGE_SIZE = 20
+    }
 }
