65cac08d467320d55cb9fc2a629ffde45a5ff6b6
Evgenii <evgeniikapchik@gmail.com>
2022-02-11 16:34:05 +0300

    work with di

--- app/src/main/java/com/example/androidtask5network/data/repository/RecipeRepositoryImpl.kt
+++ app/src/main/java/com/example/androidtask5network/data/repository/RecipeRepositoryImpl.kt
@@ -1,4 +1,20 @@
 package com.example.androidtask5network.data.repository
 
-class RecipeRepositoryImpl {
+import com.example.androidtask5network.data.network.TheCatApiService
+import com.example.androidtask5network.data.network.model.CatResponse
+import com.example.androidtask5network.domain.repository.CatsRepository
+import javax.inject.Inject
+
+class RecipeRepositoryImpl @Inject constructor(
+    private val service: TheCatApiService
+) : CatsRepository {
+    override suspend fun getCatsList(page: Int, pageSize: Int): List<CatResponse> {
+        return try {
+            val result = service.getCats(
+                page = page,
+                pageSize = pageSize
+            )
+
+        }
+    }
 }
